401e9d4b701049e7aa5dbc05746d9e0d1058a44d
xuyangluo666 <xuyangluo666@gmail.com>
2026-03-30 20:09:58 +0800

    修改ALLOWED_HOSTS配置，允许所有IP访问

--- shizaizhineng/settings.py
+++ shizaizhineng/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-^f9@*v7*h@exrj&#6wyrz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() in ('true', '1', 'yes')
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 
 # Application definition
